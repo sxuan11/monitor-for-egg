80fc133f1f7ad618f12427eb4969f6dda30000f8
sxuan <609311490@qq.com>
2020-09-21 11:25:51 +0800

    '环境配置'

--- config/config.dev.js
+++ config/config.dev.js
@@ -1,42 +1,28 @@
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
-
-/**
- * @param {Egg.EggAppInfo} appInfo app info
- */
-module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = exports = {
-    mysql: {
-      client: {
-        // host
-        host: '127.0.0.1',
-        // 端口号
-        port: '3306',
-        // 用户名
-        user: 'root',
-        // 密码
-        password: 'sxuan',
-        // 数据库名
-        database: 'shujushouji',
-      },
-      sequelize : {
-        dialect: 'mysql',
-        host: '127.0.0.1',
-        port: 3306,
-        database: 'shujushouji',
-        password: 'sxuan',
-        timezone: '+08:00' // 保存为本地时区
-      }
+module.exports = {
+  keys: '_1599446987929_1858',
+  mysql: {
+    client: {
+      // host
+      host: '127.0.0.1',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: 'sxuan',
+      // 数据库名
+      database: 'shujushouji',
     },
-
-
-  return {
-    ...config,
-    ...userConfig,
-  };
+  },
+  sequelize : {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'shujushouji',
+    password: 'sxuan',
+    timezone: '+08:00' // 保存为本地时区
+  }
 };
