076699301301bfc95f1d0645a4636f53385ae044
sxuan <609311490@qq.com>
2020-10-09 14:46:24 +0800

    功能详情

--- app/service/home/analysis/allfun.js
+++ app/service/home/analysis/allfun.js
@@ -7,14 +7,53 @@ let Op = Sequelize.Op;
 
 class Analysis extends Service {
 
-  async findAllFunByDate({date, offset, limit}) {
-    let tomorrow = moment(date).add(1, 'days').format('YYYY-MM-DD');
+  async findAllFunByDate({start_date , end_date , offset, limit}) {
+    // let tomorrow = moment(date).add(1, 'days').format('YYYY-MM-DD');
     let allFun = await this.ctx.model.Funcount.findAndCountAll({
-      where: {created_at: {[Op.gte]: date, [Op.lte]: tomorrow}},
+      where: {created_at: {[Op.gte]: start_date, [Op.lte]: end_date}},
       offset,
       limit
     })
-
+    for (let item of allFun.rows) {
+      switch (item.fun_name) {
+        case 'jiandang':
+          item.fun_name = '建档'
+          break;
+        case 'guahao':
+          item.fun_name = '挂号'
+          break;
+        case 'menzhen_pay':
+          item.fun_name = '门诊缴费'
+          break;
+        case 'print':
+          item.fun_name = '报告打印'
+          break;
+        case 'QR_signup':
+          item.fun_name = '申领电子健康码'
+          break;
+        case 'face_edit':
+          item.fun_name = '查看人脸'
+          break;
+        case 'daily_list':
+          item.fun_name = '每日清单'
+          break;
+        case 'zhuyuan_pay':
+          item.fun_name = '住院缴费'
+          break;
+        default:
+          break;
+      }
+      switch (item.is_effect) {
+        case '0':
+          item.is_effect = '未完成使用'
+          break;
+        case '1':
+          item.is_effect = '完成使用'
+          break;
+        default:
+          break;
+      }
+    }
     return allFun;
   }
 }
