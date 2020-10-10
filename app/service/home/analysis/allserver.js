389747c44bace8bc776a7638aa9a7f5b869c6676
sxuan <609311490@qq.com>
2020-10-10 11:55:39 +0800

    修该servercount，新增4个字段，数据按照创建时间倒序输出，新增过滤字段参数

--- app/service/home/analysis/allserver.js
+++ app/service/home/analysis/allserver.js
@@ -7,32 +7,97 @@ let Op = Sequelize.Op;
 
 class Analysis extends Service {
 
-  async findAllServerByDate({start_date , end_date , offset, limit,filters}) {
+  async findAllServerByDate({start_date, end_date, offset, limit, filters}) {
     // let tomorrow = moment(date).add(1, 'days').format('YYYY-MM-DD');
-    console.log(filters,'filters');
-    let request_params = ''
-    if(filters){
-      if(filters.hasOwnProperty('request_params')){
-        request_params = filters.request_params
-      }
-    }
-    let allServer = await this.ctx.model.ServerCount.findAndCountAll({
-      where: {
-        created_at:
-          {
-            [Op.gte]: start_date,
-            [Op.lte]: end_date
+    let allServer = ''
+    if (filters) {
+      if (filters.hasOwnProperty('request_params') && filters.request_params != '') {
+        allServer= await this.ctx.model.ServerCount.findAndCountAll({
+          where: {
+            created_at:
+              {
+                [Op.gte]: start_date,
+                [Op.lte]: end_date
+              },
+            request_params: {
+              [Op.substring]: filters.request_params
+            }
           },
-        request_params:{
-          [Op.substring]: request_params
-        }
+          order: [
+            ['created_at', 'DESC']
+          ],
+          offset,
+          limit
+        })
+      }else if(filters.hasOwnProperty('request_data') && filters.request_data != ''){
+        allServer= await this.ctx.model.ServerCount.findAndCountAll({
+          where: {
+            created_at:
+              {
+                [Op.gte]: start_date,
+                [Op.lte]: end_date
+              },
+            request_data: {
+              [Op.substring]: filters.request_data
+            }
+          },
+          order: [
+            ['created_at', 'DESC']
+          ],
+          offset,
+          limit
+        })
+      }else if(filters.hasOwnProperty('res_code') && filters.res_code != ''){
+        allServer= await this.ctx.model.ServerCount.findAndCountAll({
+          where: {
+            created_at:
+              {
+                [Op.gte]: start_date,
+                [Op.lte]: end_date
+              },
+            res_code: {
+              [Op.substring]: filters.res_code
+            }
+          },
+          order: [
+            ['created_at', 'DESC']
+          ],
+          offset,
+          limit
+        })
+      }else {
+        allServer= await this.ctx.model.ServerCount.findAndCountAll({
+          where: {
+            created_at:
+              {
+                [Op.gte]: start_date,
+                [Op.lte]: end_date
+              },
+          },
+          order: [
+            ['created_at', 'DESC']
+          ],
+          offset,
+          limit
+        })
+      }
+    }else {
+      allServer= await this.ctx.model.ServerCount.findAndCountAll({
+        where: {
+          created_at:
+            {
+              [Op.gte]: start_date,
+              [Op.lte]: end_date
+            },
         },
-      order: [
-        ['created_at', 'DESC']
-      ],
-      offset,
-      limit
-    })
+        order: [
+          ['created_at', 'DESC']
+        ],
+        offset,
+        limit
+      })
+    }
+
     return allServer;
   }
 
--- config/config.prod.js
+++ config/config.prod.js
@@ -10,18 +10,18 @@ module.exports = {
       // 端口号
       port: '3306',
       // 用户名
-      user: 'root',
+      user: 'lcfy_monitor',
       // 密码
       password: 'root',
       // 数据库名
-      database: 'shujushouji',
+      database: 'lcfy_monitor',
     },
   },
   sequelize : {
     dialect: 'mysql',
     host: '127.0.0.1',
     port: 3306,
-    database: 'shujushouji',
+    database: 'lcfy_monitor',
     password: 'root',
     timezone: '+08:00' ,// 保存为本地时区
     define: {
