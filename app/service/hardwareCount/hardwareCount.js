62b255af65789f5757a54770ebcabad87ae4c761
sxuan <609311490@qq.com>
2021-04-02 10:48:22 +0800

    'fix:更换提示'

--- app/service/hardwareCount/hardwareCount.js
+++ app/service/hardwareCount/hardwareCount.js
@@ -28,7 +28,7 @@ class ServerCount extends Service {
     }else{
       machine_id = this.ctx.header.hisuserid
     }
-    const err = ['纸尽','打印机未连接或未上电','打印头打开','切刀未复位','开启摄像头失败，请重试','密码键盘激活失败']
+    const err = ['打印机未连接或未上电','打印头打开','切刀未复位','卡片无效','商户号未在银行登记','查发卡方','CVN验证失败','不允许进行交易','获取医保个账支付数据异常','开启摄像头失败，请重试','黑标错误','密码键盘激活失败','打印机和调用库不匹配']
     for(let item of err){
       if(info.err_msg.includes(item)){
         const res = await this.ctx.curl(`https://oapi.dingtalk.com/robot/send?access_token=88e9f417717e0d18f5dfe479d95a6ee2118d01c17013b4539b7e9f090f77bd48&timestamp=1602225136903&sign=CRDIzGpxk7rAxdyIN6TTi7vL5ACGYe9QgJjJltL5VAY%3D`, {
